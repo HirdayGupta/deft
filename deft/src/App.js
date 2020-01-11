78632c0189003e8135531d80b8daa03f9c171d61
= <heysamlee@gmail.com>
2020-01-11 11:38:42 -0800

    added tool bar, ellipse

--- deft/src/App.js
+++ deft/src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import { Stage, Layer, Rect, Text } from "react-konva";
+import { Stage, Layer, Rect, Text, Ellipse } from "react-konva";
 import Konva from "konva";
 
-export default class ColoredRect extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { color: "red", rects: [] };
+    this.state = { color: "green", rects: [], ellipses: [] };
   }
 
   handleClick = () => {
     this.setState({
-      color: "red"
+      color: Konva.Util.getRandomColor()
     });
   };
   render() {
@@ -23,13 +23,22 @@ export default class ColoredRect extends React.Component {
       >
         <Layer>
           <Rect
+          x={0}
+          y={0}
+          width={50}
+          height={window.outerHeight}
+          stroke="black"
+          strokeWidth={1.5}
+          fill="black"
+          />
+          <Rect
             ref="draggableRectReference"
-            x={20}
-            y={20}
-            width={50}
-            height={50}
+            x={10}
+            y={240}
+            width={30}
+            height={30}
             fill={this.state.color}
-            shadowBlur={5}
+            shadowBlur={0}
             onClick={this.handleClick}
             draggable={true}
             onDragEnd={() => {
@@ -42,15 +51,47 @@ export default class ColoredRect extends React.Component {
                   {
                     x: draggableRect.getStage().getPointerPosition().x,
                     y: draggableRect.getStage().getPointerPosition().y,
-                    width: 50,
-                    height: 50,
-                    fill: "red",
+                    width: 30,
+                    height: 30,
+                    fill: "green",
                     draggable: true
                   }
                 ]
               });
               //returning draggable rect to original position
-              draggableRect.position({ x: 20, y: 20 });
+              draggableRect.position({ x: 10, y: 240 });
+              this.refs.stageReference.draw(); // or draggableRect.getStage().draw()
+            }}
+          />
+          <Ellipse
+            ref="draggableEllipseReference"
+            x={25}
+            y={200}
+            width={30}
+            height={30}
+            fill={this.state.color}
+            shadowBlur={0}
+            onClick={this.handleClick}
+            draggable={true}
+            onDragEnd={() => {
+              var draggableEllipse = this.refs.draggableEllipseReference;
+              /* adding a new rect in in state, no need to call draw() or anything
+              because updating state triggers render() again */
+              this.setState({
+                ellipses: [
+                  ...this.state.ellipses,
+                  {
+                    x: draggableEllipse.getStage().getPointerPosition().x,
+                    y: draggableEllipse.getStage().getPointerPosition().y,
+                    width: 30,
+                    height: 30,
+                    fill: "green",
+                    draggable: true
+                  }
+                ]
+              });
+              //returning draggable rect to original position
+              draggableEllipse.position({ x: 25, y: 200 });
               this.refs.stageReference.draw(); // or draggableRect.getStage().draw()
             }}
           />
@@ -65,6 +106,18 @@ export default class ColoredRect extends React.Component {
                 height={eachRect.height}
                 fill={eachRect.fill}
                 draggable={eachRect.draggable}
+              />
+            );
+          })}
+          {this.state.ellipses.map(eachEllipse => {
+            return (
+              <Ellipse
+                x={eachEllipse.x}
+                y={eachEllipse.y}
+                width={eachEllipse.width}
+                height={eachEllipse.height}
+                fill={eachEllipse.fill}
+                draggable={eachEllipse.draggable}
               />
             );
           })}
