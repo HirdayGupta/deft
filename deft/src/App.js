f84c51dbaac8d7fa8c0ebf5376a189764a9cd8db
HirdayGupta <hirday.gupta@gmail.com>
2020-01-11 17:53:17 -0800

    add transformer and custom element

--- deft/src/App.js
+++ deft/src/App.js
@@ -2,16 +2,26 @@ import React, { Component } from "react";
 import { Stage, Layer, Rect, Text, Ellipse } from "react-konva";
 import Konva from "konva";
 import TopBar from "./components/topbar";
+import RectElement from "./components/rect_element";
+import TransformerComponent from "./components/transformer";
+import EllipseElement from "./components/ellipse_element";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { color: "blue", rects: [], ellipses: [] };
+    this.state = { color: "blue", rects: [], ellipses: [], selectedShapeName: ""};
+    this.shapeCount = 0;
   }
 
   handleClick = () => {
     this.setState({
       color: Konva.Util.getRandomColor()
+    });
+  };
+
+  handleStageClick = e => {
+    this.setState({
+      selectedShapeName: e.target.name()
     });
   };
 
@@ -25,10 +35,12 @@ export default class App extends React.Component {
           width: 30,
           height: 30,
           fill: this.state.color,
-          draggable: true
+          draggable: true,
+          name: "shape"+this.shapeCount
         }
       ]
     });
+    this.shapeCount++;
   }
 
   addEllipse = (newX, newY) => {
@@ -41,10 +53,12 @@ export default class App extends React.Component {
           width: 30,
           height: 30,
           fill: this.state.color,
-          draggable: true
+          draggable: true,
+          name: "shape"+this.shapeCount
         }
       ]
     });
+    this.shapeCount++;
   }
 
   render() {
@@ -55,32 +69,38 @@ export default class App extends React.Component {
           width={window.innerWidth}
           height={window.innerHeight}
           ref="stageReference"
+          onClick={this.handleStageClick}
         >
         <Layer>
           {this.state.rects.map(eachRect => {
             return (
-              <Rect
+              <RectElement
                 x={eachRect.x}
                 y={eachRect.y}
                 width={eachRect.width}
                 height={eachRect.height}
                 fill={eachRect.fill}
                 draggable={eachRect.draggable}
+                name={eachRect.name}
               />
             );
           })}
           {this.state.ellipses.map(eachEllipse => {
             return (
-              <Ellipse
+              <EllipseElement
                 x={eachEllipse.x}
                 y={eachEllipse.y}
                 width={eachEllipse.width}
                 height={eachEllipse.height}
                 fill={eachEllipse.fill}
                 draggable={eachEllipse.draggable}
+                name={eachEllipse.name}
               />
             );
           })}
+          <TransformerComponent
+            selectedShapeName={this.state.selectedShapeName}>
+          </TransformerComponent>
         </Layer>
       </Stage>
       </React.Fragment>
---
+++ deft/src/components/rect_element.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { Stage, Layer, Rect, Transformer, Text, Ellipse } from "react-konva";
+import Konva from "konva";
+
+export default class RectElement extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      x: props.x,
+      y: props.y,
+      width: props.width,
+      height: props.height,
+      fill: props.fill
+    };
+  }
+
+  handleDragEnd = (e) => {
+    this.setState({
+      x: e.target.x(),
+      y: e.target.y()
+    });
+  }
+
+  handleTransformEnd = (e) => {
+    // transformer is changing scale of the node
+    // and NOT its width or height
+    // but in the store we have only width and height
+    // to match the data better we will reset scale on transform end
+    const node = this.shapeRef;
+    const scaleX = node.scaleX();
+    const scaleY = node.scaleY();
+
+    // we will reset it back
+    node.scaleX(1);
+    node.scaleY(1);
+
+    this.setState({
+      x: node.x(),
+      y: node.y(),
+      width: Math.max(5, node.width() * scaleX),
+      height: Math.max(5, node.height() * scaleY)
+    });
+  }
+
+  render() {
+    return(
+      <React.Fragment>
+      <Rect
+        x={this.state.x}
+        y={this.state.y}
+        ref={ref => {this.shapeRef = ref}}
+        width={this.state.width}
+        height={this.state.height}
+        fill={this.state.fill}
+        name={this.props.name}
+        draggable={true}
+        onClick={this.handleClick}
+        onDragEnd={this.handleDragEnd}
+        onTransformEnd={this.handleTransformEnd}
+      />
+      </React.Fragment>
+
+    )
+  }
+}
